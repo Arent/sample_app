565ea8747bdd86bf84a6574fe31944b46cf03d45
Arent Stienstra <Arent.stienstra@gmail.com>
2014-02-09 13:24:16 +0100

    Swedish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Arent Stienstra"
+    email    "arent.stienstra@gmail.com"
+    password "q1W@e3R$"
+    password_confirmation "q1W@e3R$"
+  end
+end
