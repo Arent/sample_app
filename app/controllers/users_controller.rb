0958848d6728d91804f37eb91f36370bd86726c9
Arent <arent.stienstra@gmail.com>
2014-02-10 13:27:25 +0100

    Werkend tot Show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_action :signed_in_user, only: [:edit, :update]
+before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -20,8 +21,7 @@ before_action :signed_in_user, only: [:edit, :update]
     end
   end
   
-	def edit
-    @user = User.find(params[:id])
+  def edit
   end
 
  def update
@@ -46,9 +46,18 @@ before_action :signed_in_user, only: [:edit, :update]
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 	# before filters
-def signed_in_user
+    def signed_in_user
+      unless signed_in?
+      store_location
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
     end
- 
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
